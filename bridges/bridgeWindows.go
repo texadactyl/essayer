514caed8baaa96792e6dccf2a8c9795d361fe375
Richard Elkins <richard.elkins@gmail.com>
2024-08-26 12:58:35 -0500

    fix type returned by LoadLibrary

--- bridges/bridgeWindows.go
+++ bridges/bridgeWindows.go
@@ -8,11 +8,9 @@ import (
 )
 
 func ConnectLibrary(libPath string) uintptr {
-	var handle uintptr
-	var err error
-	handle, err = windows.LoadLibrary(libPath)
+	handle, err := windows.LoadLibrary(libPath)
 	if err != nil {
-		log.Fatalf("ConnectLibrary: purego.Dlopen for [%s] failed, reason: [%s]\n",
+		log.Fatalf("ConnectLibrary: windows.LoadLibrary for [%s] failed, reason: [%s]\n",
 			libPath, err.Error())
 	}
 	return handle
